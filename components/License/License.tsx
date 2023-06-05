770eca6f4389ec42059f24757f310fca8ba589ac
Alexander Nikolayev <alexnikolayev.dev@gmail.com>
2023-06-05 15:58:07 +0300

    feat: add contract images to license component

--- components/License/License.tsx
+++ components/License/License.tsx
@@ -1,20 +1,41 @@
 import styles from "./License.module.scss";
 import Image from "next/image";
+import dogovor from "./assets/dogovor.png";
 
 export const License = () => {
   return (
     <>
       <section className={styles.license}>
-        <h2>Действуем на основании лицензии МЧС</h2>
+        <h2>
+          Действуем на основании лицензии МЧС. Работаем по договору,
+          предоставляем гарантию.
+        </h2>
         <div className={styles.license_center}>
           <Image
             className={styles.license__license}
             src="/lic1.webp"
             width={300}
             height={400}
-            alt=""
+            alt="Лицензия МЧС. ООО Пожконтроль. Лицевая сторона"
           />
-          <Image src="/lic2.webp" width={300} height={400} alt="" />
+          <Image
+            src="/lic2.webp"
+            width={300}
+            height={400}
+            alt="Лицензия МЧС. ООО Пожконтроль. Обратная сторона"
+          />
+          <Image
+            src={dogovor}
+            width={300}
+            height={400}
+            alt="Пример договора. ООО Пожконтроль. Первая страница"
+          />
+          <Image
+            src={dogovor}
+            width={300}
+            height={400}
+            alt="Пример договора. ООО Пожконтроль. Страница с гарантийными обязательствами"
+          />
         </div>
       </section>
     </>
