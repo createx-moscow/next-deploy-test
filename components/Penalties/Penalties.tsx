8cd1f49ec881295b70b906e439742ce6430c254f
Alexander Nikolayev <alexnikolayev.dev@gmail.com>
2023-06-14 01:36:17 +0300

    feat: add penalties sections wrappers

--- components/Penalties/Penalties.tsx
+++ components/Penalties/Penalties.tsx
@@ -7,19 +7,24 @@ export const Penalties = () => {
     <section>
       <h2>Почему важно соблюдать требования пожарной безопасности</h2>
       <div className={styles.penalties__wrapper}>
-        <h3>За нарушение может грозить:</h3>
-        <ul className={styles.penalties__list}>
-          <li>Штраф для ИП и должностных лиц - до 50 000 рублей</li>
-          <li>Штраф для юридических лиц - до 500 000 рублей</li>
-          <li>Приостановка деятельности до 90 дней</li>
-        </ul>
-        <Image
-          src={molotok}
-          width={1065}
-          height={600}
-          alt="молоток судьи с пачкой денег"
-          className={styles.penalties__image}
-        />
+        <div>
+          <h3>За нарушение может грозить:</h3>
+          <ul className={styles.penalties__list}>
+            <li>Штраф для ИП и должностных лиц - до 50 000 рублей</li>
+            <li>Штраф для юридических лиц - до 500 000 рублей</li>
+            <li>Приостановка деятельности до 90 дней</li>
+          </ul>
+        </div>
+
+        <div>
+          <Image
+            src={molotok}
+            width={1065}
+            height={600}
+            alt="молоток судьи с пачкой денег"
+            className={styles.penalties__image}
+          />
+        </div>
       </div>
     </section>
   );
